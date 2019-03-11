2c08d818e47fa0087d0e0c26b35817c7743d0b05
Olga Novik <olga.v.novik@gmail.com>
2019-03-11 13:46:24 +0300

    Start develop my application

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <h1>Начинаем разработку</h1>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      
+    })
+  };
+</script>
+
+<style>
+
+</style>
